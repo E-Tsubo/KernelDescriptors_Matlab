47dcc3d23875c1bffdcda5b0bda228add1417f2f
hideshi <hideshi@2d721cdd-3942-4a6a-b24e-6b8ad5b5fa91>
2012-11-26 11:34:29 +0000

    TMP
    git-svn-id: svn://localhost/KernelDescriptors_Matlab@53 2d721cdd-3942-4a6a-b24e-6b8ad5b5fa91

---
+++ PBM/calcPBM.m
@@ -0,0 +1,17 @@
+%% 2012/11/26 Written by Hideshi T.
+
+disp( 'Please input rgb, dep image' );
+
+for i = 1:USE_PART_MODEL
+    
+    [tmp_dec, tmp_label, tmp_features, tmp_name] = process_PartBasedModel2( PBM.partdetector, i, rgb, dep );
+    
+    part_decvalues{i} = tmp_dec;
+    part_label{i} = tmp_label;
+end
+
+[ final_dec, final_label ] = combineModel( part_decvalues, part_label, PBM.combinedetector.svm, PBM.combinedetector.minvalue, PBM.combinedetector.maxvalue ); 
+
+disp( ' ' );
+disp( [ 'Detected Object is ' tmp_name{final_label} ] );
+disp( final_dec );
