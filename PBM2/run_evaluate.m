5de9e359e75067f0860e7842c0a3aae0ae42a6ac
hideshi <hideshi@2d721cdd-3942-4a6a-b24e-6b8ad5b5fa91>
2012-12-09 07:12:34 +0000

    TMP
    git-svn-id: svn://localhost/KernelDescriptors_Matlab@67 2d721cdd-3942-4a6a-b24e-6b8ad5b5fa91

--- PBM2/run_evaluate.m
+++ PBM2/run_evaluate.m
@@ -2,11 +2,11 @@ for loop = 1:5
     rootname = 'evaluate_'; 
     extension = '.mat';
 
-    dir{1} = '../images/Model_Test/left/';
-    dir{2} = '../images/Model_Test/right/';
-    dir{3} = '../images/Model_Test/top/';
-    dir{4} = '../images/Model_Test/bottom/';
-    dir{5} = '../images/Model_Test/full/';
+    dir{1} = '../images/rgbdsubset/rgbdsubset_left/';
+    dir{2} = '../images/rgbdsubset/rgbdsubset_right/';
+    dir{3} = '../images/rgbdsubset/rgbdsubset_top/';
+    dir{4} = '../images/rgbdsubset/rgbdsubset_bottom/';
+    dir{5} = '../images/rgbdsubset/rgbdsubset_full/';
     imdir = dir{loop};
     
     %% Part-based Model
