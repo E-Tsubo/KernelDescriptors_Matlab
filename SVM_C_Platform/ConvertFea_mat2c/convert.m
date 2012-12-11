7b0c4ba1c2f4116c7c4bfd14079be55491b5d2df
hideshi <hideshi@2d721cdd-3942-4a6a-b24e-6b8ad5b5fa91>
2012-12-11 09:52:23 +0000

    Add:liblinear with openmp at c platform.
    git-svn-id: svn://localhost/KernelDescriptors_Matlab@79 2d721cdd-3942-4a6a-b24e-6b8ad5b5fa91

--- SVM_C_Platform/ConvertFea_mat2c/convert.m
+++ SVM_C_Platform/ConvertFea_mat2c/convert.m
@@ -6,3 +6,7 @@ trainhmp=trainhmp';
 trainhmp=sparse(double(trainhmp));
 
 libsvmwrite('output.txt', rgbdclabel, trainhmp);
+
+%% About minvalue, maxvalue, 
+%% I fixed calc kdes program.
+%% So, when program saves rgbdfea.mat, this feature file store.
